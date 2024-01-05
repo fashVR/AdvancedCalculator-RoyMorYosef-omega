6881e08226a96fb52e994ce2d5b978f7b072a638
fashVR <myroy2005@gmail.com>
2024-01-05 15:41:13 +0200

    program layouting, branching test

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-# This is the main program file for the Advanced Calculator
 
+def calc(expression):
+    postfixed = postfix(expression)
+    calced = calculate(postfixed)
+    return calced
+
+
+def receive_input():
+    while True:
+        expression = input()
+        try:
+            result = calc(expression)
+            print(result)
+        except ValueError as e:
+            print(e)
